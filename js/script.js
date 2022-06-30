7f861abf03590c5793ed3bfbc1d4f1874f911879
bardachyounes <bardach.younes@gmail.com>
2022-06-30 15:11:43 +0100

    Build add, remove and read buttons

--- js/script.js
+++ js/script.js
@@ -8,9 +8,7 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.readChange = function () {
-  if ((this.read = "read")) {
-    this.read = "unread";
-  } else this.read = "read";
+  this.read = !this.read;
 };
 
 const addButton = document.querySelector(".form>.blue-button");
@@ -32,14 +30,22 @@ addButton.addEventListener(
       <input class="align-checkbox" type="checkbox" id="read"/>
       </div>`
     );
+    addButton.insertAdjacentHTML(
+      "afterend",
+      `<div class="hidden-alert">Please fill all fields above</div>`
+    );
     addButton.addEventListener("click", () => {
       const titleInput = document.querySelector("#title").value;
       const authorInput = document.querySelector("#author").value;
       const pagesInput = document.querySelector("#pages").value;
       const readInput = document.querySelector("#read").checked;
-      addBookToLibrary(titleInput, authorInput, pagesInput, readInput);
-      console.log(readInput);
-      displayBooks();
+      if (titleInput && authorInput && pagesInput) {
+        addBookToLibrary(titleInput, authorInput, pagesInput, readInput);
+        displayBooks();
+        addButton.nextElementSibling.classList.add("hidden-alert");
+      } else {
+        addButton.nextElementSibling.classList.remove("hidden-alert");
+      }
     });
   },
   { once: true }
@@ -56,58 +62,60 @@ function displayBooks() {
     const infoCard = document.createElement("div");
     infoCard.classList.add("card");
     cards.appendChild(infoCard);
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 9; i++) {
       infoCard.appendChild(document.createElement("div"));
     }
     const infos = [
-      `<span class = "bold-text">Title:</span> ${book.title}`,
-      `<span class = "bold-text">Author:</span> ${book.author}`,
-      `<span class = "bold-text">Pages:</span> ${book.pages}`,
-      `<span class = "bold-text">Read: </span><svg style="width:24px;height:24px; position: relative; top: 0.5rem;" viewBox="0 0 24 24">
-      <path fill="green" d="M9,20.42L2.79,14.21L5.62,11.38L9,14.77L18.88,4.88L21.71,7.71L9,20.42Z" />
-      </svg>`,
-      `<span class = "bold-text">Read: </span><svg style="width:24px;height:24px; position: relative; top: 0.5rem;" viewBox="0 0 24 24">
+      `<span class = "bold-text">Title:</span>`,
+      `${book.title}`,
+      `<span class = "bold-text">Author: </span>`,
+      `${book.author}`,
+      `<span class = "bold-text">Pages: </span>`,
+      `${book.pages}`,
+      `<span class = "bold-text">Read:</span>`,
+      `<svg style="width:24px;height:24px;" viewBox="0 0 24 24">
       <path fill="red" d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/>
       </svg>`,
+      `<svg style="width:24px;height:24px;" viewBox="0 0 24 24">
+      <path fill="green" d="M9,20.42L2.79,14.21L5.62,11.38L9,14.77L18.88,4.88L21.71,7.71L9,20.42Z" />
+      </svg>`,
     ];
-    for (let i = 1, j = 0; i < 5, j < 3; i++, j++) {
+    for (let i = 1, j = 0; i < 8, j < 7; i++, j++) {
       infoCard.querySelector(`.card>:nth-child(${i})`).innerHTML = infos[j];
     }
     if (!book.read) {
-      infoCard.querySelector(`.card>:nth-child(4)`).innerHTML = infos[5];
       infoCard.querySelector(
-        ".card>:nth-child(5)"
+        ".card>:nth-child(9)"
       ).innerHTML = `<button class="blue-button">Remove</button> <button class="blue-button">Read</button>`;
+      infoCard.querySelector(`.card>:nth-child(8)`).innerHTML = infos[7];
       infoCard
-        .querySelector(".card>:nth-child(5)>:last-child")
+        .querySelector(".card>:nth-child(9)>:last-child")
         .addEventListener("click", () => {
-          infoCard.querySelector(
-            `.card>:nth-child(4)`
-          ).innerHTML = `<span class = "bold-text">Read: </span><svg style="width:24px;height:24px; position: relative; top: 0.5rem;" viewBox="0 0 24 24">
-          <path fill="green" d="M9,20.42L2.79,14.21L5.62,11.38L9,14.77L18.88,4.88L21.71,7.71L9,20.42Z" />
-          </svg>`;
-          infoCard.querySelector(`.card>:nth-child(4)`).innerHTML = infos[4];
-          infoCard.querySelector(
-            ".card>:nth-child(5)"
-          ).innerHTML = `<button class="blue-button">Remove</button> <button class="blue-button">Unread</button>`;
+          book.readChange();
+          displayBooks();
+        });
+      infoCard
+        .querySelector(".card>:nth-child(9)>:first-child")
+        .addEventListener("click", () => {
+          myLibrary.splice(myLibrary.indexOf(book), 1);
+          displayBooks();
         });
     } else {
-      infoCard.querySelector(`.card>:nth-child(4)`).innerHTML = infos[4];
       infoCard.querySelector(
-        ".card>:nth-child(5)"
+        ".card>:nth-child(9)"
       ).innerHTML = `<button class="blue-button">Remove</button> <button class="blue-button">Unread</button>`;
+      infoCard.querySelector(`.card>:nth-child(8)`).innerHTML = infos[8];
       infoCard
-        .querySelector(".card>:nth-child(5)>:last-child")
+        .querySelector(".card>:nth-child(9)>:last-child")
         .addEventListener("click", () => {
-          infoCard.querySelector(
-            `.card>:nth-child(4)`
-          ).innerHTML = `<span class = "bold-text">Read: </span><svg style="width:24px;height:24px; position: relative; top: 0.5rem;" viewBox="0 0 24 24">
-          <path fill="red" d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/>
-          </svg>`;
-          infoCard.querySelector(`.card>:nth-child(4)`).innerHTML = infos[5];
-          infoCard.querySelector(
-            ".card>:nth-child(5)"
-          ).innerHTML = `<button class="blue-button">Remove</button> <button class="blue-button">Read</button>`;
+          book.readChange();
+          displayBooks();
+        });
+      infoCard
+        .querySelector(".card>:nth-child(9)>:first-child")
+        .addEventListener("click", () => {
+          myLibrary.splice(myLibrary.indexOf(book), 1);
+          displayBooks();
         });
     }
   });
